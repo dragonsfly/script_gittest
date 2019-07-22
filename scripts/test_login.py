61f900d1f1a90d7688a6f743e532c938ae474727
dragonsfly <xinglong_work@163.com>
2019-07-22 10:22:42 +0800

    first commit

---
+++ scripts/test_login.py
@@ -0,0 +1,26 @@
+# from base.base_driver import init_driver
+
+
+class TestLogin:
+
+
+    # def setup(self):
+    #     self.driver = init_driver()
+
+    # def test_login1(self):
+        # self.driver.find_element_by_xpath("//*[@text='更多']").click()
+
+    def test_login2(self):
+        assert 1
+
+    def test_login3(self):
+        assert 1
+
+    def test_login4(self):
+        assert 1
+
+    def test_login5(self):
+        assert 1
+
+    def test_login6(self):
+        assert 0
